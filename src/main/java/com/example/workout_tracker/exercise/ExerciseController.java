977c23466cd61876bf8f1c97fd8d295ba6dea454
Filip-Alves <alvesfilip.work@gmail.com>
2025-09-17 17:42:30 +0200

    add getAllExercise API

--- src/main/java/com/example/workout_tracker/exercise/ExerciseController.java
+++ src/main/java/com/example/workout_tracker/exercise/ExerciseController.java
@@ -2,10 +2,10 @@ package com.example.workout_tracker.exercise;
 
 import com.example.workout_tracker.exercise.dto.CreateExerciseRequest;
 import com.example.workout_tracker.exercise.dto.ExerciseResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/exercises")
@@ -24,5 +24,14 @@ public class ExerciseController {
         Exercise savedExercise = exerciseRepository.save(exerciseToSave);
 
         return ExerciseMapper.toExerciseResponse(savedExercise);
+    }
+
+    @GetMapping
+    public List<ExerciseResponse> getAllExercises() {
+        List<Exercise> exercisesFromDb = exerciseRepository.findAll();
+
+        return exercisesFromDb.stream()
+                .map(ExerciseMapper::toExerciseResponse)
+                .toList();
     }
 }
